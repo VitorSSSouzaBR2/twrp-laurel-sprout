1b7a2a52a92cd8176cf3552da1dc42bb25617169
iHemal <i.mominulislamhemal@gmail.com>
2021-10-23 01:19:13 +0000

    laurel_sprout: soong namespace

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 LOCAL_PATH := device/xiaomi/laurel_sprout
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
+
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
