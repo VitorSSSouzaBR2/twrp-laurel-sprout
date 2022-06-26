c41e0cba2d16a8cb4b8990840c613885e6f1ed96
endbugger <islammominul.ihemal@gmail.com>
2022-06-26 12:20:05 +0200

    Initial Adaption for A12

--- device.mk
+++ device.mk
@@ -67,6 +67,6 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so
 
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/laurel_sprout/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+#PRODUCT_COPY_FILES += \
+#    $(OUT_DIR)/target/product/laurel_sprout/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
